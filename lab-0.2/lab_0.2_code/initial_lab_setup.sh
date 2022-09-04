e4c03144712a40c4c086790fbd17c264d6b66be0
William-An <China_Aisa@live.com>
2022-09-04 15:27:08 -0400

    Remove lab 0.2 and lab 1 code as we have moved them into individual repos

--- lab-0.2/lab_0.2_code/initial_lab_setup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Copy FVP to desktop and install extensions for vscode
-cp -r /local/scratch/a/FVP_Linux ~/Desktop/
-code --install-extension dan-c-underwood.arm
-code --install-extension ms-vscode.cpptools
-code --install-extension webfreak.debug
--- lab-0.2/lab_0.2_code/lab0_setup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-ROOT_DIR=$(readlink -f `dirname $0`)
-
-cd "$ROOT_DIR"
-code .
-make launch_fvp &
